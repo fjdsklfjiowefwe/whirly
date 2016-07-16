23edb7cdd9bc4f06105681b608d58aae7308c94a
Greg Smith <greg@gregdev.com.au>
2016-07-17 09:16:00 +0930

    cleanup

--- app/src/main/java/com/gregdev/whirldroid/setup/steps/ThemeStep.java
+++ app/src/main/java/com/gregdev/whirldroid/setup/steps/ThemeStep.java
@@ -1,23 +1,13 @@
 package com.gregdev.whirldroid.setup.steps;
 
 import android.content.SharedPreferences;
-import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v4.widget.CompoundButtonCompat;
-import android.support.v7.widget.AppCompatRadioButton;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.RadioButton;
-import android.widget.TextView;
 
-import com.github.fcannizzaro.materialstepper.AbstractStep;
 import com.gregdev.whirldroid.R;
 import com.gregdev.whirldroid.Whirldroid;
-import com.gregdev.whirldroid.setup.SteppedSetup;
-
-import java.util.ArrayList;
 
 public class ThemeStep extends SetupStep {
 
@@ -31,7 +21,7 @@ public class ThemeStep extends SetupStep {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
-        final SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getContext());
+        final SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(Whirldroid.getContext());
 
         if (Integer.parseInt(settings.getString("pref_theme", "0")) == Whirldroid.DARK_THEME) {
             RadioButton light   = (RadioButton) view.findViewById(R.id.theme_light);
