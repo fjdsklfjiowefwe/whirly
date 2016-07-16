25f1aab04562f9e146329097579c653e2de015ea
Greg Smith <greg@gregdev.com.au>
2016-07-16 10:55:45 +0930

    notifications setup screen now does the things

--- app/src/main/java/com/gregdev/whirldroid/setup/steps/NotificationsStep.java
+++ app/src/main/java/com/gregdev/whirldroid/setup/steps/NotificationsStep.java
@@ -3,6 +3,11 @@ package com.gregdev.whirldroid.setup.steps;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.gregdev.whirldroid.R;
@@ -16,6 +21,42 @@ public class NotificationsStep extends SetupStep {
 
         setLayoutResource(R.layout.setup_step_notifications);
         setStepTitle("Notifications");
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        final SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getContext());
+        final SharedPreferences.Editor settingsEditor = settings.edit();
+
+        view.findViewById(R.id.notify_whims).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                settingsEditor.putBoolean("pref_whimnotify", ((CheckBox) v).isChecked());
+                settingsEditor.apply();
+            }
+        });
+
+        view.findViewById(R.id.notify_threads).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                settingsEditor.putBoolean("pref_watchednotify", ((CheckBox) v).isChecked());
+                settingsEditor.apply();
+            }
+        });
+
+        ((Spinner) view.findViewById(R.id.notify_freq)).setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String[] frequencyValues = getResources().getStringArray(R.array.values_pref_notifyfreq);
+                settingsEditor.putString("pref_notifyfreq", frequencyValues[position]);
+                settingsEditor.apply();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     @Override
