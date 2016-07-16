1e31e9e67134545c9204df5995c6637fad0c53bc
Greg Smith <greg@gregdev.com.au>
2016-07-16 20:45:45 +0930

    track initial screen

--- app/src/main/java/com/gregdev/whirldroid/setup/steps/IntroStep.java
+++ app/src/main/java/com/gregdev/whirldroid/setup/steps/IntroStep.java
@@ -2,7 +2,10 @@ package com.gregdev.whirldroid.setup.steps;
 
 import android.os.Bundle;
 
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
 import com.gregdev.whirldroid.R;
+import com.gregdev.whirldroid.Whirldroid;
 
 public class IntroStep extends SetupStep {
 
@@ -12,6 +15,12 @@ public class IntroStep extends SetupStep {
 
         setLayoutResource(R.layout.setup_step_intro);
         setStepTitle("Whirldroid");
+
+        // Obtain the shared Tracker instance.
+        Whirldroid application = (Whirldroid) getActivity().getApplication();
+        Tracker mTracker = application.getDefaultTracker();
+        mTracker.setScreenName("Setup: Start");
+        mTracker.send(new HitBuilders.ScreenViewBuilder().build());
     }
 
 }
